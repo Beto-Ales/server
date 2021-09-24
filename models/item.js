722e3e7173895cac7943f0c4b4a98d2a1f7b0333
Alberto Alesandroni <betoalesandroni@gmail.com>
2021-09-24 19:28:09 +0200

    mongoose schema

---
+++ models/item.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const itemSchema = mongoose.Schema({
+    name: String,
+    checked: Boolean
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+export default Item;
